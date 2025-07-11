b68cad68a5a9bb099467a9ae7b579745e6cf4e65
Martin Landa <landa.martin@gmail.com>
2025-07-11 14:01:03 +0200

    happy new year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Radiation Toolbox Reader'
-copyright = u'2016-2024, SURO and OpenGeoLabs'
+copyright = u'2016-2025, SURO and OpenGeoLabs'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
